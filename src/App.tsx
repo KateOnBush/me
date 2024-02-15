b5dbaab2937ed972f596e1331d29d845d0368461
KateOnBush <aouabsss@gmail.com>
2024-02-15 22:50:08 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="w-full h-full center-items flex justify-center items-center
+      bg-gradient-to-br hover:bg-gradient-to-tr from-sky-800 to-indigo-950">
+        <p className="text-5xl font-bold text-center text-white">My name is Aouab</p>
+      </div>
+  );
+}
+
+export default App;
